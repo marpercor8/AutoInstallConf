15511085f36ef5718ee6680bfcd7b03220133d81
meco <meco@meco.localdomain>
2022-11-13 18:56:02 +0100

    Variables

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from os.path import expanduser
-
+import signal
 
 LOCAL_DIRECTORY = os.getcwd()
 USER_DIRECTORY = expanduser("~")
@@ -10,10 +10,63 @@ USER_DIRECTORY = expanduser("~")
 PACKAGE_LIST = "sources/packages.txt"
 CONFIG_LIST_HOMEDIR  = "sources/path_config_home.txt"
 CONFIG_LIST_DIR = "sources/path_config_dir.txt"
+
 CONFIG_LIST_DIR_SOURCE = "dotfiles/.config"
 CONFIG_LIST_HOMEDIR_SOURCE ="dotfiles"
-def main():
-    pass
+
+
+def signal_handler(signum, frame):
+    print("Are you sure stopping? [s/N]:")
+    res = input()
+    if res == "s" or res == "S":
+        sys.exit(0)
+
+
+def main(arguments):
+    try:
+        arg = arguments[1]
+    except:
+        print("ERROR: Parse error, try -h")
+        sys.exit()
+
+    try:
+        if(arg=="-i"):
+            print("Installing configuration")
+            install()
+        elif(arg=="-l"):
+            print("Saving configuration")
+            load_config_user()
+        elif(arg=="-h"):
+            print_help()
+        else:
+            print("ERROR")
+    except:
+            print("ERROR: Ha ocurrido un error en el proceso")
+
+def print_help():
+    print("AUTO INSTALL CONF\n")
+    print("use: python main.py [options]\n")
+    print("-h: Display this")
+    print("-i: Install configuration places in dotfiles")
+    print("-l: Load configuration in dotfiles")
+    print("\n\n")
+
+
+def load_config_user():
+    print("Copying user dir into {}/{}\n".format(LOCAL_DIRECTORY,CONFIG_LIST_HOMEDIR_SOURCE))
+    copy_config_dir_user()
+    copy_config_user_homedir()
+    print("Copying all packages into {}\n".format(PACKAGE_LIST).upper())
+    copy_all_packages()
+    print("Done.") 
+
+
+def install():
+    print("Copying all configurations files into {}".format(CONFIG_LIST_HOMEDIR).upper())
+    install_config()
+    print("Installing all packages from {}".format(PACKAGE_LIST).upper())
+    install_packages()
+    print("Done.") 
 
 
 def install_packages():
@@ -21,6 +74,10 @@ def install_packages():
         for l in f:
             os.system("trizen -Syua {}".format(l))
 
+def install_config():
+    first_dir = "{}/{}".format(LOCAL_DIRECTORY, CONFIG_LIST_HOMEDIR_SOURCE)
+    second_dir = "{}".format(USER_DIRECTORY)
+    os.system("cp -rf {} {}".format(first_dir, second_dir))
 
 
 def copy_config_dir_user():
@@ -30,6 +87,7 @@ def copy_config_dir_user():
             second_dir = "{}/{}/{}".format(LOCAL_DIRECTORY, CONFIG_LIST_DIR_SOURCE, config.replace("\n", ""))
 
             os.system("rm -rf {}".format(second_dir))
+
             os.system("cp -rf {} {}".format(first_dir, second_dir))
 
 def copy_config_user_homedir():
@@ -45,11 +103,8 @@ def copy_all_packages():
 
 
 
-def load_config_user():
-    copy_config_dir_user()
-    copy_config_user_homedir()
-    copy_all_packages()
 
 if __name__ == '__main__':
-    load_config_user()
+    signal.signal(signal.SIGINT, signal_handler)
+    main(sys.argv)
 
